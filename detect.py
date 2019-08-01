f16a83b2604d76e06916cb86df4f25eed584407a
szemenyeim <szemenyeim@gmail.com>
2019-08-01 18:56:54 +0200

    Detect works

--- detect.py
+++ detect.py
@@ -1,20 +1,25 @@
 import torch
+import torchvision
 import torchvision.transforms as transforms
 import numpy as np
 import glob
 import cv2
 import sys
-
+import progressbar
 
 root = "E:/Traffic/trafficSigns" if sys.platform == 'win32' else "./data"
+
 net = torch.load(root + '/model.pth')
 
-transform_val = transforms.Compose([
+transform = transforms.Compose([
         transforms.Normalize((0.49139968, 0.48215827, 0.44653124),
                              (0.24703233, 0.24348505, 0.26158768))
     ])
 
+names = {11:"Bike",52:"No Turn",49:"Parking",45:"Stop",36:"Crossing",17:"One way",8:"Road work",9:"Traffic Lights",22:"Roundabout",43:"Crossing"}
+
 if __name__ == '__main__':
+    #val(1)
     path = "./Images/"
 
     for img_name in glob.glob1(path,"*.JPG"):
@@ -33,24 +38,23 @@ if __name__ == '__main__':
         ROIs = [cv2.boundingRect(c) for c in contours if cv2.contourArea(c) > 1000]
 
         for ROI in ROIs:
-            x1 = ROI[0]-12
-            x2 = ROI[0]+ROI[2]+12
-            y1 = ROI[1]-12
-            y2 = ROI[1]+ROI[3]+12
+            x1 = ROI[0]-15
+            x2 = ROI[0]+ROI[2]+15
+            y1 = ROI[1]-15
+            y2 = ROI[1]+ROI[3]+15
 
             imgROI = cv2.cvtColor(cv2.resize(np.copy(img[y1:y2,x1:x2]),(32,32)),cv2.COLOR_BGR2RGB)
-            imgROI = torch.Tensor(imgROI).permute(2,0,1)
-            imgROI = torch.unsqueeze(transform_val(imgROI),0).cuda()
+            imgROI = torch.Tensor(imgROI).permute(2,0,1)/255.0
+            imgROI = torch.unsqueeze(transform(imgROI),0).cuda()
 
             out = net(imgROI)
 
             _,pred = torch.max(out,0)
 
-            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),1)
-            cv2.putText(img,"%d"%pred.item(),(x1,y1),cv2.FONT_HERSHEY_COMPLEX_SMALL,1.0,(255,0,255))
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),2)
+            cv2.putText(img,names[pred.item()],(x1,y1-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,1.0,(0,255,0))
 
 
-        cv2.imshow("Bin",img_bin)
         cv2.imshow("Image",img)
         ret = cv2.waitKey(0)
         if ret == 27:
--- train.py
+++ train.py
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     # Data augmentation
     transform = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.2),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.ToTensor(),
         transforms.Normalize((0.49139968, 0.48215827, 0.44653124),
                              (0.24703233, 0.24348505, 0.26158768))
